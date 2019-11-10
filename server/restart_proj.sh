4d59a8315c5bbdf720da7089a020722b7d50c2a0
leon <leiyang_ace@163.com>
2019-11-10 17:57:35 +0800

    修改拼写错误

--- server/restart_proj.sh
+++ server/restart_proj.sh
@@ -2,13 +2,13 @@
 echo "正在重启项目。。。"
 
 echo "web 服务器重启。。。"
-supervisorctl reatart uwsgi;
-supervisorctl reatart daphne;
+supervisorctl restart uwsgi;
+supervisorctl restart daphne;
 echo "web 服务器已启动"
 
 echo "celery 重启。。。"
-/etc/init.d/celeryd reatart;
-/etc/init.d/celerybeat reatart;
+/etc/init.d/celeryd restart;
+/etc/init.d/celerybeat restart;
 echo "celery 已启动"
 
 echo "项目已启动"

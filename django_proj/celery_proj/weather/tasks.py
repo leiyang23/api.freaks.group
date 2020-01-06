6b711107790556ff34bc9ea5dd72ca4fa1e411d3
leon <leiyang_ace@163.com>
2020-01-06 20:58:35 +0800

    天气定时任务发送邮件提醒时缺少主题参数

--- django_proj/celery_proj/weather/tasks.py
+++ django_proj/celery_proj/weather/tasks.py
@@ -3,13 +3,13 @@ from celery_proj.settings import logger
 from celery_proj.tasks.common import send_email
 
 from celery_proj.weather.util import tomorrow_weather
-from celery_proj.weather.setting import SAFE_WEATHER,USERS
+from celery_proj.weather.setting import SAFE_WEATHER, USERS
 
 
 @app.task
 def weather():
     for user in USERS:
-        weather, temp_change = tomorrow_weather(user['address'])
-        logger.debug(weather, temp_change)
-        if weather not in SAFE_WEATHER or abs(temp_change) >= 5:
-            send_email.apply_async(([user['email']], weather+ "\n温差：" + str(temp_change)))
+        weather_desc, temp_change = tomorrow_weather(user['address'])
+        logger.debug(weather_desc, temp_change)
+        if weather_desc not in SAFE_WEATHER or abs(temp_change) >= 5:
+            send_email.apply_async(([user['email']], "天气提醒", weather_desc + "\n温差：" + str(temp_change)))

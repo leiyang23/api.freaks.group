5b56238cfe9674a6dfd6fbd90697dbb3590cdf86
leon <leiyang_ace@163.com>
2020-02-16 18:18:29 +0800

    admin 添加 修改头像（地址）

--- django_proj/apps/user/admin.py
+++ django_proj/apps/user/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
 from django.utils.html import format_html
 
@@ -16,6 +17,15 @@ class CustomUserAdmin(UserAdmin):
     image.short_description = "头像"
 
     list_display = ('image', 'username', 'email', 'is_staff', 'gender')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        ("头像", {'fields': ('avatar', )})
+    )
 
 
 admin.site.site_header = "网站后台管理"
